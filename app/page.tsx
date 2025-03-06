4aeba7efb8cf4c1c47ecdb5c57e61ea208eec18f
Abin Antony <abina5448@gmail.com>
2025-03-06 12:48:53 +0530

    main_push

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,96 @@
 import Image from "next/image";
+import { collection, getDocs, getFirestore, orderBy, query } from 'firebase/firestore';
+import { app } from "@/firebase";
 
-export default function Home() {
+type MyDataType = {
+  college: string;
+  level: number,
+  stream: string;
+  trophies: number,
+  user_name:string;
+  user_email:string;
+  user_phone:string;
+  year: number;
+}
+
+export default async function Home() {
+  async function getAllDocuments<T>(collectionName: string): Promise<T[]> {
+    const q = query(collection(db, collectionName), orderBy('trophies', 'desc'));
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })) as T[];
+  }
+
+  async function getMyCollection() {
+    try {
+      const data:MyDataType[] = await getAllDocuments<MyDataType>('myCollection') as MyDataType[];
+      return data;
+    } catch (error: any) {
+      console.error('Error fetching documents:', error);
+      return [];
+    }
+  }
+
+  const db = getFirestore(app);
+  const data: MyDataType[] = await getAllDocuments<MyDataType>("gamers");
+  console.log(data);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="flex items-start flex-col justify-items-center min-h-screen px-8 py-2 pb-20 gap-16 sm:px-10 sm:py-10 ">
+      <main className="flex w-full justify-between items-center gap-8 ">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          className=""
+          src="/logo.png"
+          alt="logo"
           width={180}
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <Image
+          aria-hidden
+          src="/leaderboardTitle.png"
+          alt="File icon"
+          className="w-auto sm:flex hidden h-12"
+          width={100}
+          height={100}
+        />
+        <Image
+          aria-hidden
+          src="/asthra.png"
+          alt="File icon"
+          className="w-auto h-12"
+          width={100}
+          height={100}
+        />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <Image
+        aria-hidden
+        src="/leaderboardTitle.png"
+        alt="File icon"
+        className="w-auto sm:hidden flex h-12"
+        width={100}
+        height={100}
+      />
+      <section>
+        <h1 className="sm:text-3xl text-xl font-bold text-yellow-400">
+          Welcome to the Arcade Showdown
+        </h1>
+        <p className="sm:text-lg text-sm mt-2 text-gray-400">
+          A collection of classic arcade games
+        </p>
+      </section>
+      <section className="w-full flex flex-col items-center">
+        <h2 className="sm:text-2xl text-xl font-bold text-yellow-400">
+          Leaderboard
+        </h2>
+        <ul className="w-full mt-4 space-y-4">
+          {data.map((row, index) => (
+            <li key={index} className="flex justify-between gap-2 items-center p-4 bg-gray-800 rounded-lg">
+              <span className="text-yellow-400 font-bold">{index + 1}</span>
+              <span className="text-white text-center">{row?.user_name+", "+row?.stream+", "+(row?.college.length > 20 ? row?.college.substring(0,18): row?.college)}</span>
+              <span className="text-yellow-400 font-bold">{row.trophies}</span>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
